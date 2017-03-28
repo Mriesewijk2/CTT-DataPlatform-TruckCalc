e765531d2f1a20f1ca437edba3a7822c78c8f4e0
Mriesewijk2 <m.riesewijk@student.utwente.nl>
2017-03-28 13:53:29 +0200

    voegt reactiveTable package toe en implementeert deze

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -4,7 +4,13 @@ import { TruckOrders } from '../api/truckOrders.js';
 import './body.html';
 
 Template.body.helpers({
-  truckOrders () {
-    return TruckOrders.find({ arrival_time: { $exists: true, $ne: null}});
+  settings: function () {
+    var collection = TruckOrders.find({ arrival_time: { $exists: true, $ne: null}});
+    return {
+      collection: collection,
+      rowsPerPage: 10,
+      showFilter: true,
+      fields: ['t_number', 'arrival_time', 'number']
+    };
   }
 });
