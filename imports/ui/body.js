8bec6dedc7e12b651b50effede6317d09c132e50
Mriesewijk2 <m.riesewijk@student.utwente.nl>
2017-03-28 13:22:57 +0200

    Eerste werkende versie van het ophalen van data

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,12 +1,10 @@
 import { Template } from 'meteor/templating';
-
 import { TruckOrders } from '../api/truckOrders.js';
 
 import './body.html';
 
-
 Template.body.helpers({
-  tasks () {
-    return TruckOrders.find({});
+  truckOrders () {
+    return TruckOrders.find({ arrival_time: { $exists: true, $ne: null}});
   }
 });
