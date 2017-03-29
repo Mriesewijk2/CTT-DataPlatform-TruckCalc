a4e11d2773bd0d4e8c97b9c5f726d84d2d742392
Mriesewijk2 <m.riesewijk@student.utwente.nl>
2017-03-29 11:50:12 +0200

    voegt comments toe

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -3,14 +3,4 @@ import { TruckOrders } from '../api/truckOrders.js';
 
 import './body.html';
 
-Template.body.helpers({
-  settings: function () {
-    var collection = TruckOrders.find({ arrival_time: { $exists: true, $ne: null}});
-    return {
-      collection: collection,
-      rowsPerPage: 10,
-      showFilter: true,
-      fields: ['t_number', 'arrival_time', 'number']
-    };
-  }
-});
+import './table.js';
---
+++ imports/ui/table.js
@@ -0,0 +1,19 @@
+import { Template } from 'meteor/templating';
+import { TruckOrders } from '../api/truckOrders.js';
+
+// JS code for the table
+Template.body.helpers({
+  settings: function () {
+    var collection = TruckOrders.find({ arrival_time: { $exists: true, $ne: null}});
+    return {
+      collection: collection,
+      rowsPerPage: 100,
+      showFilter: true,
+      fields: [
+        'container',
+        'needed_depart_time',
+        'arrival_time',
+        'city']
+    };
+  }
+});
