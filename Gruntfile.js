697c32b98c5a94d755b5787cbaeafd815f5a1210
Mriesewijk2 <m.riesewijk@student.utwente.nl>
2017-03-29 13:43:54 +0200

    grunt en parameters

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+
+var loader = require('load-grunt-config-data')(grunt);
+
+// initialize config
+var config = {pkg: grunt.file.readJSON('./package.json')};
+
+// load configuration (if some file exports an fn() a deep clone of `config` is provided to it)
+grunt.util._.merge(config, loader.load('config/parameters.json'));
+grunt.initConfig(config);
+
+grunt.initConfig({
+  mongoimport: {
+    options: {
+      db : config.dev.db,
+      host : config.dev.url, //optional
+      port: config.host, //optional
+      username : config.dev.user, //optional
+      password : config.dev.pwd,  //optional
+      stopOnError : false,  //optional
+      collections : config.collections
+    }
+  }
+});
+
+grunt.loadNpmTasks('grunt-mongoimport');
+
+};
