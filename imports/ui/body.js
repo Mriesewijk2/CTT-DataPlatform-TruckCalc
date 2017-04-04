5046de135dcb3042d0c8a7f5a9afafdf462e8c79
Mriesewijk2 <m.riesewijk@student.utwente.nl>
2017-04-04 15:31:13 +0200

    Eerste versie prototype af

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,15 +1,23 @@
 import { Template } from 'meteor/templating';
 import { TruckOrders } from '../api/truckOrders.js';
+import { moment } from 'meteor/momentjs:moment';
 
 import './body.html';
 import './table.js';
 import './truckOrderForm.html';
 
+Template.addTruckOrderForm.onRendered(function () {
+  this.$('.datetimepicker').datetimepicker({
+    format: 'HH:mm DD-MM-YYYY'
+  });
+});
+
 Template.addTruckOrderForm.events({
   'submit form': function (event) {
     event.preventDefault();
     var target = event.target;
-    var arrivalTime = target.ordernumber.value;
+    //  moment does not work correctly with date from datetimepicker, therefore the new Date
+    var arrivalTime = moment(new Date(target.arrival_time.value)).format();
     TruckOrders.insert({
       booking: target.ordernumber.value,
       arrival_time: arrivalTime,
@@ -17,10 +25,10 @@ Template.addTruckOrderForm.events({
       load_disc: target.destination.value,
       from: target.destination.value
     });
-    event.target.ordernumber.value = '';
+    event.target.reset();
   }
 });
 
 function departTimeCalc (arrivalTime) {
-  return 1152;
+  return moment(arrivalTime).subtract(2, 'hours').format();
 }
