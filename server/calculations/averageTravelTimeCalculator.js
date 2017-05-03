5183e75bf86231a719c0ca34f1eca1800fecfb3e
Mriesewijk2 <m.riesewijk@student.utwente.nl>
2017-05-03 10:57:17 +0200

    First version of psuedocode implemented

--- server/calculations/averageTravelTimeCalculator.js
+++ server/calculations/averageTravelTimeCalculator.js
@@ -1,4 +1,4 @@
-import { averageTravelTimes, CWReturnVehicles, truckPlanning } from '../../lib/collections.js';
+import { averageTravelTimes, CWReturnVehicles, truckPlanning, CWReturnVehiclePosition } from '../../lib/collections.js';
 import { Meteor } from 'meteor/meteor';
 
 if (Meteor.isServer) {
@@ -6,26 +6,53 @@ if (Meteor.isServer) {
     var truckPlannings = getTruckPlannings(departCode, destinationCode).fetch();
     console.log('trigger');
     console.log(truckPlannings.length);
-    for(i = 0; i < truckPlannings.length; i++){
-      var truck = truckPlannings[i].Truck;
-      var vehicle = getCWVehicleID(truck);
-      if(vehicle){
-        console.log(vehicle);
-      }
-
+    var totalTime;
+    var count;
+    for (i = 0; i < truckPlannings.length; i++) {
+      var vehicleID = getCWVehicleID(truckPlannings[i].Truck);
+      var vehiclePositions = getCWVehiclePostion(vehicleID, truckPlannings[i].DepartTime, truckPlannings[i].ReturnTime );
+      for ( j = 0; j < vehiclePositions.length; j++) {
+  			if (inRange(vehiclePositions[j].Longitude, vehiclePositions[j].Latitude, end.lon, end.lat)){
+  				totalTime += vehiclePositions[j].ReceivedTime - vehiclePositions[0].ReceivedTime;
+          count++;
+  			}
+  		}
+  	return -1337;
     }
     return;
   };
 
+  // get the CarrierWeb vehicle id from the license entered in Modality
   function getCWVehicleID (license) {
-    try {
-      return CWReturnVehicles.findOne({ License: license }).CWVehicleID;
-    } catch (e) {
+    var cwVehicle = CWReturnVehicles.findOne({ License: license });
+    if (cwVehicle) {
+      return cwVehicle.CWVehicleID;
+    } else {
       return;
     }
   }
 
-  function getTruckPlannings (departCode, destinationCode) {
+  // get the truck Plannings from the 
+  function getTruckPlannings (departCode, destinationCode, returnCode) {
     return truckPlanning.find({ From: departCode, LoadDisch: destinationCode });
+  }
+
+  function getCWVehiclePostion (CWVehicleID, departTime, returnTime) {
+    return CWVehiclePositions.find({CWVehicleID: CWVehicleID, ReceivedTime: {$gt: departTime-2, $lt: returnTime+2} }).fetch();
+  }
+
+  // genakt van http://www.movable-type.co.uk/scripts/latlong.html
+  function inRange (lonpos, latpos, lonclient, latclient) {
+  	var R = 6371e3; // metres
+  	var φ1 = latpos.toRadians();
+  	var φ2 = latclient.toRadians();
+  	var Δφ = (latclient-latpos).toRadians();
+  	var Δλ = (lonclient-lonpos).toRadians();
+
+  	var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) + Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ/2) * Math.sin(Δλ/2);
+  	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+  	var d = R * c;
+  	return d < 100;
   }
 }
